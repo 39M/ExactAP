1111e1310b36f9a6f2c4628c310d2290d29f1b32
Jiaqi Wang <xwszc7@gmail.com>
2016-02-26 15:36:37 +0800

    support lock operation counter value

--- app/src/main/java/com/m39/exactap/MainActivity.java
+++ app/src/main/java/com/m39/exactap/MainActivity.java
@@ -1,34 +1,44 @@
 package com.m39.exactap;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
-import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
+// TODO: Layout fix
+// TODO: More operation
+// TODO: Beautify, list? card view?
 
 public class MainActivity extends AppCompatActivity {
+    private static final int op_num = 9;
+
     private EditText current_ap_text;
     private EditText target_ap_text;
-    private TextView multi_cf_count;
-    private TextView cf_count;
-    private TextView capture_count;
-    private TextView complete_count;
-    private TextView link_count;
-    private TextView deploy_count;
-    private TextView hack_count;
-    private TextView upgrade_count;
-    private TextView recharge_count;
+    private TextView[] count_views = new TextView[op_num];
+
+    Drawable lock_icon;
 
     private int ap_difference;
+    private String[] op_names = new String[]{
+            "Double Control Field", "Control Field", "Capture", "Complete", "Link", "Deploy", "Hack", "Upgrade", "Recharge"
+    };
     private int[] ap_obtains = new int[]{2813, 1563, 625, 375, 313, 125, 100, 65, 10};
-    private int[] op_counters = new int[9];
+    private int[] op_counters = new int[op_num];
+    private boolean[] op_lock = new boolean[op_num];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +49,22 @@ public class MainActivity extends AppCompatActivity {
         current_ap_text = (EditText) findViewById(R.id.current_ap_input);
         target_ap_text = (EditText) findViewById(R.id.target_ap_input);
 
-        multi_cf_count = (TextView) findViewById(R.id.multi_cf_count_label);
-        cf_count = (TextView) findViewById(R.id.cf_count_label);
-        capture_count = (TextView) findViewById(R.id.capture_count_label);
-        complete_count = (TextView) findViewById(R.id.complete_count_label);
-        link_count = (TextView) findViewById(R.id.link_count_label);
-        deploy_count = (TextView) findViewById(R.id.deploy_count_label);
-        hack_count = (TextView) findViewById(R.id.hack_count_label);
-        upgrade_count = (TextView) findViewById(R.id.upgrade_count_label);
-        recharge_count = (TextView) findViewById(R.id.recharge_count_label);
+        count_views[0] = (TextView) findViewById(R.id.multi_cf_count_label);
+        count_views[1] = (TextView) findViewById(R.id.cf_count_label);
+        count_views[2] = (TextView) findViewById(R.id.capture_count_label);
+        count_views[3] = (TextView) findViewById(R.id.complete_count_label);
+        count_views[4] = (TextView) findViewById(R.id.link_count_label);
+        count_views[5] = (TextView) findViewById(R.id.deploy_count_label);
+        count_views[6] = (TextView) findViewById(R.id.hack_count_label);
+        count_views[7] = (TextView) findViewById(R.id.upgrade_count_label);
+        count_views[8] = (TextView) findViewById(R.id.recharge_count_label);
+
+        // get drawable
+        lock_icon = ContextCompat.getDrawable(this, R.drawable.ic_lock);
+        lock_icon.setBounds(0, 0, lock_icon.getMinimumWidth(), lock_icon.getMinimumHeight());
 
         // init UI
+        fill_counters_to(-1);
         set_text();
 
         // create text watcher
@@ -67,11 +82,11 @@ public class MainActivity extends AppCompatActivity {
                 update_solution();
             }
         };
-        // add text watcher for edit text
+        // add text watcher for text editing
         current_ap_text.addTextChangedListener(textWatcher);
         target_ap_text.addTextChangedListener(textWatcher);
 
-        // clear focus when action done
+        // TODO: clear focus when action done
 //        target_ap_text.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 //            @Override
 //            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -100,10 +115,108 @@ public class MainActivity extends AppCompatActivity {
         moveTaskToBack(true);
     }
 
+    private void set_text() {
+        for (int i = 0; i < op_num; i++) {
+            // set text to counter value or '?'
+            count_views[i].setText(String.format(getString(R.string.ap_multiply), op_counters[i] != -1 ? op_counters[i] + "" : "?"));
+
+            if (!op_lock[i])
+                // remove lock drawable
+                count_views[i].setCompoundDrawables(null, null, null, null);
+            else
+                // set lock drawable
+                count_views[i].setCompoundDrawables(null, null, lock_icon, null);
+        }
+
+    }
+
+    // TODO: show tips when tap operation name
+    public void show_tips(final View view) {
+
+    }
+
+    public void pick_number(final View view) {
+        // get view id
+        final AtomicInteger view_index = new AtomicInteger(-1);
+        for (int i = 0; i < op_num; i++)
+            if (count_views[i].getId() == view.getId()) {
+                view_index.set(i);
+                break;
+            }
+        if (view_index.get() == -1) {
+            Log.w("View not found", "View ID: " + view.getId());
+            return;
+        }
+
+        // create dialog
+        final AppCompatDialog dialog = new AppCompatDialog(this, R.style.DialogTheme);
+        dialog.setTitle(op_names[view_index.get()]);
+        dialog.setContentView(R.layout.number_picker_dialog);
+
+        // picker setup
+        final NumberPicker picker = (NumberPicker) dialog.findViewById(R.id.number_picker);
+        picker.setMinValue(0);
+        picker.setMaxValue(ap_difference / ap_obtains[view_index.get()]);
+        picker.setValue(op_counters[view_index.get()] != -1 ? op_counters[view_index.get()] : 0);
+//        picker.setWrapSelectorWheel(false);
+//        picker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//            @Override
+//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+//                Log.v(oldVal+"old", newVal+"new");
+//            }
+//        });
+
+        // buttons setup
+        Button lock_button = (Button) dialog.findViewById(R.id.pick_number_button);
+        Button unlock_button = (Button) dialog.findViewById(R.id.reset_number_button);
+        Button cancel_button = (Button) dialog.findViewById(R.id.cancel_pick_number_button);
+        // lock button on click listener
+        lock_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // apply input immediately
+                picker.clearFocus();
+                // lock value
+                op_lock[view_index.get()] = true;
+                op_counters[view_index.get()] = picker.getValue();
+
+                update_solution();
+                dialog.dismiss();
+            }
+        });
+        // show or not show unlock button
+        if (!op_lock[view_index.get()])
+            // remove unlock button
+            ((ViewGroup) unlock_button.getParent()).removeView(unlock_button);
+        else
+            // unlock button on click listener
+            unlock_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // unlock value
+                    op_lock[view_index.get()] = false;
+
+                    update_solution();
+                    dialog.dismiss();
+                }
+            });
+        // cancel button on click listener
+        cancel_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    // update solution and UI
     private void update_solution() {
         // empty input, reset counters
         if (current_ap_text.getText().toString().length() < 1 || target_ap_text.getText().toString().length() < 1) {
-            Arrays.fill(op_counters, 0);
+            ap_difference = 0;
+            fill_counters_to(-1);
             set_text();
             return;
         }
@@ -112,59 +225,62 @@ public class MainActivity extends AppCompatActivity {
         ap_difference = Integer.parseInt(target_ap_text.getText().toString()) -
                 Integer.parseInt(current_ap_text.getText().toString());
         // reset conters
-        Arrays.fill(op_counters, 0);
+        fill_counters_to(0);
+        // get ap differece left after substract locked operations
+        int ap_left = ap_difference;
+        for (int i = 0; i < op_num; i++)
+            if (op_lock[i])
+                ap_left -= op_counters[i] * ap_obtains[i];
         // try to get a solution
-        if (ap_difference < 0 || !get_solution(ap_difference, 0)) {
+        if (ap_left < 0 || !search_solution(ap_left, 0)) {
             // if faild, set counters to -1
-            Arrays.fill(op_counters, -1);
+            fill_counters_to(-1);
         }
-        /*********/
+        /*TODO: Remove this part before final build*/
         else if (!is_right_answer())
             Log.w("Warning", "Wrong answer");
-        /*********/
+        /*TODO: Remove this part before final build*/
 
         // update UI
         set_text();
     }
 
-    private void set_text() {
-        multi_cf_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[0], op_counters[0]));
-        cf_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[1], op_counters[1]));
-        capture_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[2], op_counters[2]));
-        complete_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[3], op_counters[3]));
-        link_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[4], op_counters[4]));
-        deploy_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[5], op_counters[5]));
-        hack_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[6], op_counters[6]));
-        upgrade_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[7], op_counters[7]));
-        recharge_count.setText(String.format(getString(R.string.ap_multiply), ap_obtains[8], op_counters[8]));
-    }
-
-    private boolean get_solution(int ap_left, int pos) {
+    // search for solution
+    private boolean search_solution(int ap_left, int op_index) {
         // got solution, return
         if (ap_left == 0) {
             return true;
         }
 
         // failed to get solution
-        if (pos == ap_obtains.length - 1 && ap_left % ap_obtains[pos] != 0) {
+        if (op_index == op_num - 1 && ap_left % ap_obtains[op_index] != 0) {
             return false;
         }
 
         // dfs to get solution
-        if (pos <= 8) {
-            for (int i = ap_left % ap_obtains[pos]; i <= ap_left; i += ap_obtains[pos]) {
-                op_counters[pos] = (ap_left - i) / ap_obtains[pos];
-                if (get_solution(i, pos + 1))
+        if (op_index < op_num) {
+            if (op_lock[op_index])
+                return search_solution(ap_left, op_index + 1);
+            for (int i = ap_left % ap_obtains[op_index]; i <= ap_left; i += ap_obtains[op_index]) {
+                op_counters[op_index] = (ap_left - i) / ap_obtains[op_index];
+                if (search_solution(i, op_index + 1))
                     return true;
             }
         }
         return false;
     }
 
+    // assign all unlocked counters with x
+    private void fill_counters_to(int x) {
+        for (int i = 0; i < op_num; i++)
+            if (!op_lock[i])
+                op_counters[i] = x;
+    }
+
+    // check answer
     private boolean is_right_answer() {
         int sum = 0;
-        // check answer
-        for (int i = 0; i < op_counters.length; i++)
+        for (int i = 0; i < op_num; i++)
             sum += op_counters[i] * ap_obtains[i];
         return sum == ap_difference;
     }
